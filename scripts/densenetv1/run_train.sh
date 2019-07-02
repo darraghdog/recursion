09f5e70df7f0cdeeee7f054a2b6527d82fabb0c7
darraghdog <darragh.hanley@gmail.com>
2019-07-02 21:16:31 +0100

    Densenet starter

---
+++ scripts/densenetv1/run_train.sh
@@ -0,0 +1,5 @@
+N_GPU=1
+
+bsub  -q lowpriority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=docker.com/darraghdog/darraghdog/kaggle:pytorch_build \
+            -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/recursion/scripts/densenetv1  && python3 train.py --epochs 30 \
+            --lr 0.001 --batchsize 16 --workpath scripts/densenetv1 --weightsname pytorch_model_densenet.bin"
