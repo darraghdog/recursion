444e036e843fa4f4882398bc5726a578ca51524a
darraghdog <darragh.hanley@gmail.com>
2019-07-11 01:47:58 -0500

    utils

---
+++ utils.py
@@ -0,0 +1,11 @@
+import pickle
+
+def dumpobj(file, obj):
+    with open(file, 'wb') as handle:
+        pickle.dump(obj, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
+def loadobj(file):
+    with open(file, 'rb') as handle:
+        return pickle.load(handle)
+
+
