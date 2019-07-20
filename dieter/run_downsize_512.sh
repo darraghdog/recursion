4cb75c1d4bf1008cea274ce5cdae76ecdb49129a
darraghdog <darragh.hanley@gmail.com>
2019-07-20 12:52:53 -0500

    Cgan 512

--- dieter/run_downsize_512.sh
+++ dieter/run_downsize_512.sh
@@ -2,8 +2,8 @@ N_GPU=1
 WDIR='dieter'
 
 bsub  -q lowpriority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=docker.optum.com/dhanley2/bert:pytorch_build \
-            -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/recursion/$WDIR  && python3 stats_6channel.py  \
-            --dimsize 128 --datapath  mount/128X128X6"
+            -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/recursion/$WDIR  && python3 downsize_6channel.py  \
+            --targetpath mount/512X512X6 --dimsize 512"
 
 #bsub  -q lowpriority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=docker.optum.com/dhanley2/bert:pytorch_build \
 #            -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/recursion/$WDIR  && python3 downsize.py  \
