feb124143c3000f99ed97456fb8145d148a1030e
dhanley2 <darragh_hanley@optum.com>
2019-07-21 22:26:58 +0100

    Rebalancing coefs

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
+# https://github.com/PavelOstyakov/predictions_balancing/blob/master/run.py
 import pickle
+import argparse
+import os
+import torch
+import tqdm
 
 def dumpobj(file, obj):
     with open(file, 'wb') as handle:
@@ -9,3 +14,37 @@ def loadobj(file):
         return pickle.load(handle)
 
 
+def _get_predicts(predicts, coefficients):
+    return torch.einsum("ij,j->ij", (predicts, coefficients))
+
+
+def _get_labels_distribution(predicts, coefficients):
+    predicts = _get_predicts(predicts, coefficients)
+    labels = predicts.argmax(dim=-1)
+    counter = torch.bincount(labels, minlength=predicts.shape[1])
+    return counter
+
+
+def _compute_score_with_coefficients(predicts, coefficients):
+    counter = _get_labels_distribution(predicts, coefficients).float()
+    counter = counter * 100 / len(predicts)
+    max_scores = torch.ones(len(coefficients)).cuda().float() * 100 / len(coefficients)
+    result, _ = torch.min(torch.cat([counter.unsqueeze(0), max_scores.unsqueeze(0)], dim=0), dim=0)
+
+    return float(result.sum().cpu())
+
+
+def _find_best_coefficients(predicts, coefficients, alpha=0.001, iterations=100):
+    best_coefficients = coefficients.clone()
+    best_score = _compute_score_with_coefficients(predicts, coefficients)
+
+    for _ in tqdm.trange(iterations):
+        counter = _get_labels_distribution(predicts, coefficients)
+        label = int(torch.argmax(counter).cpu())
+        coefficients[label] -= alpha
+        score = _compute_score_with_coefficients(predicts, coefficients)
+        if score > best_score:
+            best_score = score
+            best_coefficients = coefficients.clone()
+
+    return best_coefficients
