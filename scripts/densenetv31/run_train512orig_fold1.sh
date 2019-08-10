189b41b206a723dcb124a69f3b580b58e9341512
darraghdog <darragh.hanley@gmail.com>
2019-08-10 05:57:56 -0500

    Change cbind

---
+++ scripts/densenetv31/run_train512orig_fold1.sh
@@ -0,0 +1,10 @@
+N_GPU=1
+WDIR='densenetv31'
+FOLD=1
+SIZE='512'
+
+
+bsub  -q lowpriority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:apex_build \
+            -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/recursion/scripts/$WDIR  && python3 trainorig.py  \
+            --logmsg Recursion-concat-$SIZE-fp16 --nbags 20  --epochs 100 --fold $FOLD  --lr 0.000025 --lrmult 20  --batchsize 16  --workpath scripts/$WDIR  \
+            --probsname probs_$SIZE  --cutmix_prob 1.0 --precision half  --beta 1.0  --imgpath data/mount/512X512X6/ --weightsname weights/pytorch_cut_model_512_densenet$FOLD.bin"
