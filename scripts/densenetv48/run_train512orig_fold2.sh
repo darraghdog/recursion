51828d242708a5daf667c140d8047b1e47f4d0ed
dhanley2 <darragh_hanley@optum.com>
2019-09-05 19:15:59 +0100

    Finetuning

---
+++ scripts/densenetv48/run_train512orig_fold2.sh
@@ -0,0 +1,11 @@
+N_GPU=1
+WDIR='densenetv48'
+FOLD=2
+SIZE='512'
+FILTER='U2'
+
+bsub  -q lowpriority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:apex_build \
+            -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/recursion/scripts/$WDIR  && python3 trainorig.py  \
+            --logmsg Recursion-finetune-$FILTER-$SIZE-fp16 --nbags 20  --epochs 100 --fold $FOLD  --lr 0.00025 --batchsize 16  --workpath scripts/$WDIR  \
+            --xtrasteps 100 --probsname probs_$SIZE  --cutmix_prob 1.0 --precision half  --beta 1.0  --imgpath data/mount/512X512X6/ \
+            --exp_filter $FILTER --dump_emb 0 --weightsname weights/pytorch_$FILTER_512_densenet$FOLD.bin"
